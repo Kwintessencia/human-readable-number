3067dc8dd35752fb5e24f8c3d1ef021c66189365
Kwintessencia <alex.sukhanova92@gmail.com>
2023-01-08 19:22:25 +0300

    refactor: change algorithm

--- src/index.js
+++ src/index.js
@@ -11,39 +11,13 @@ let findDozens= (number) => {
       str = str + unit[number.charAt(1)];
    } else if (number.length == 2 && number[0] == 1){
       str = str + unit[number];
-   } else if (number.length == 2 && number[0] == 2 && number[1] == 0){
-      str = str + dozens[0];
-   } else if (number.length == 2 && number[0] == 2 && number[1] > 0){
-      str = str + `${dozens[0]} ${unit[number.charAt(1)]}`;
-   } else if (number.length == 2 && number[0] == 3 && number[1] == 0){
-      str = str + dozens[1];
-   } else if (number.length == 2 && number[0] == 3 && number[1] > 0){
-      str = str + `${dozens[1]} ${unit[number.charAt(1)]}`;
-   } else if (number.length == 2 && number[0] == 4 && number[1] == 0){
-      str = str + dozens[2];
-   } else if (number.length == 2 && number[0] == 4 && number[1] > 0){
-      str = str + `${dozens[2]} ${unit[number.charAt(1)]}`;
-   } else if (number.length == 2 && number[0] == 5 && number[1] == 0){
-      str = str + dozens[3];
-   } else if (number.length == 2 && number[0] == 5 && number[1] > 0){
-      str = str + `${dozens[3]} ${unit[number.charAt(1)]}`;
-   } else if (number.length == 2 && number[0] == 6 && number[1] == 0){
-      str = str + dozens[4];
-   } else if (number.length == 2 && number[0] == 6 && number[1] > 0){
-      str = str + `${dozens[4]} ${unit[number.charAt(1)]}`;
-   } else if (number.length == 2 && number[0] == 7 && number[1] == 0){
-      str = str + dozens[5];
-   } else if (number.length == 2 && number[0] == 7 && number[1] > 0){
-      str = str + `${dozens[5]} ${unit[number.charAt(1)]}`;
-   } else if (number.length == 2 && number[0] == 8 && number[1] == 0){
-      str = str + dozens[6];
-   } else if (number.length == 2 && number[0] == 8 && number[1] > 0){
-      str = str + `${dozens[6]} ${unit[number.charAt(1)]}`;
-   } else if (number.length == 2 && number[0] == 9 && number[1] == 0){
-      str = str + dozens[7];
-   } else if (number.length == 2 && number[0] == 9 && number[1] > 0){
-      str = str + `${dozens[7]} ${unit[number.charAt(1)]}`;
-   } return str;
+   } else if (number.length == 2 && number[1] == 0){
+      str = str + dozens[number[0]-2];
+   } else if (number.length == 2 && number[1] > 0){
+      str = str + `${dozens[number[0]-2]} ${unit[number.charAt(1)]}`;
+   }
+    return str;
+
 }
 let findHundred = () => {
    str ='';
